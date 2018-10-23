7af23bed0f3e9f741d65fe9e16b766d77a05b7bc
guruchian <30399464+semicon@users.noreply.github.com>
2018-10-23 12:01:14 +0700

    quiz01

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1540269996,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sendbtn-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/falsebtn-sheet0.png",
+		"images/falsebtn-sheet1.png",
+		"images/truebtn-sheet0.png",
+		"images/truebtn-sheet1.png",
+		"images/bgquiz-sheet0.png",
+		"images/header-sheet0.png",
+		"images/bgscore-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
